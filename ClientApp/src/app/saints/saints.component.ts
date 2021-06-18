cd1178cb87b8967eecd6be033b3249fb1841c0fd
John <rendeiroj@live.com>
2021-06-18 15:11:17 -0400

    added Saints

---
+++ ClientApp/src/app/saints/saints.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saints',
+  templateUrl: './saints.component.html'
+})
+export class SaintsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
