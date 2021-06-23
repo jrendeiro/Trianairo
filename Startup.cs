c883de82f7eb1cd78d37f1abe6bce55f0bf2f380
John <rendeiroj@live.com>
2021-06-22 23:45:30 -0400

    moved UseProxyToSpaDevelopmentServer in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -65,11 +65,11 @@ namespace Trianairo
 
                 spa.Options.SourcePath = "ClientApp";
 
-                spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
 
                 if (env.IsDevelopment())
                 {
                     spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
