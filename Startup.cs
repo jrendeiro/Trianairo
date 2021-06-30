4e02e078274da14d7be80e62e3ddaefc78811c0b
John <rendeiroj@live.com>
2021-06-30 09:15:30 -0400

    configured for TLS mutual auth

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Authentication.Certificate;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
@@ -23,6 +25,20 @@ namespace Trianairo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            
+            // Configure the application to use the protocol and client ip address forwared by the frontend load balancer
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+            
+            // Configure the application to client certificate forwarded the frontend load balancer
+        
+            services.AddCertificateForwarding(options => { options.CertificateHeader = "X-ARR-ClientCert"; });
+
+            // Add certificate authentication so when authorization is performed the user will be created from the certificate
+            services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate();            
+            
             services.AddCors();
 
             // services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
@@ -51,7 +67,12 @@ namespace Trianairo
                 app.UseHsts();
             }
 
+            app.UseForwardedHeaders();
+            app.UseCertificateForwarding();
+
             app.UseHttpsRedirection();
+
+
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
@@ -59,6 +80,10 @@ namespace Trianairo
             }
 
             app.UseRouting();
+            
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
