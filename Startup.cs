e17a61563a21f2bd2ce1f5a5a25edf412a41d824
John <rendeiroj@live.com>
2021-07-22 01:03:44 -0400

    uncomment app.useSpa in startup

--- Startup.cs
+++ Startup.cs
@@ -74,19 +74,16 @@ namespace Trianairo
                     pattern: "{controller=Admin}/{action=Index}");
             });
 
-            // app.UseSpa(spa =>
-            // {
-            //     // To learn more about options for serving an Angular SPA from ASP.NET Core,
-            //     // see https://go.microsoft.com/fwlink/?linkid=864501
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
 
-            //     spa.Options.SourcePath = "ClientApp";
-
-            //     if (env.IsDevelopment())
-            //     {
-            //         spa.UseAngularCliServer(npmScript: "start");
-            //         spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
-            //     }
-            // });
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                }
+            });
         }
     }
 }
