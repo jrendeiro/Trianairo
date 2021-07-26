3e0083112b8387375f05549cc3eb6b5961c51024
John <rendeiroj@live.com>
2021-07-26 12:22:16 -0400

    added Saints columns

---
+++ Migrations/20210726161422_Add_columns_to_Saints.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trianairo.Migrations
+{
+    public partial class Add_columns_to_Saints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "beatifiedDate",
+                table: "Saints",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "birthDate",
+                table: "Saints",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "canonizedDate",
+                table: "Saints",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "country",
+                table: "Saints",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "deathDate",
+                table: "Saints",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "feastDay",
+                table: "Saints",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "beatifiedDate",
+                table: "Saints");
+
+            migrationBuilder.DropColumn(
+                name: "birthDate",
+                table: "Saints");
+
+            migrationBuilder.DropColumn(
+                name: "canonizedDate",
+                table: "Saints");
+
+            migrationBuilder.DropColumn(
+                name: "country",
+                table: "Saints");
+
+            migrationBuilder.DropColumn(
+                name: "deathDate",
+                table: "Saints");
+
+            migrationBuilder.DropColumn(
+                name: "feastDay",
+                table: "Saints");
+        }
+    }
+}
--- Models/saint.cs
+++ Models/saint.cs
@@ -7,5 +7,13 @@ public class Saint {
     public string pictureUrl { get; set; }
     public string quote { get; set; }
     public string biography { get; set; }
+    public string country { get; set; }
     public string status { get; set; }
+    public DateTime? beatifiedDate { get; set; }
+    public DateTime? canonizedDate { get; set; }
+    public DateTime? birthDate { get; set; }
+    public DateTime? deathDate { get; set; }
+    public DateTime? feastDay { get; set; }
+    
+
 }
