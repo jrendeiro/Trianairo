8bbd22202c6a09be5c2c6a0d7463b018ecf3a0be
John <rendeiroj@live.com>
2021-08-03 14:56:15 -0400

    saint controller - modify url for saints order

--- Controllers/SaintsController.cs
+++ Controllers/SaintsController.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Options;
 using Trianairo.Data;
 using System.Linq.Dynamic.Core;
 using AutoMapper;
+using System.ComponentModel;
 
 namespace Trianairo.Controllers
 {
@@ -41,20 +42,30 @@ namespace Trianairo.Controllers
             _cloudinary = new Cloudinary(acc);
         }
 
-        [HttpGet("/saintsapi/{field}/{orderby}")]
-        public IEnumerable<SaintDto> GetSaints(string field, string orderby)
+        [HttpGet("/saintsapi/{property}/{order}")]
+        public IEnumerable<SaintDto> GetSaints(string property, Order order)
         {
+            // var saints = _context.Saints.ToList();
 
-            // SYNTAX: OrderBy("Make ASC, Year DESC")
-            var saints = _context.Saints.AsQueryable().OrderBy($"{field} {orderby}").ToArray();
+            PropertyDescriptor prop = TypeDescriptor.GetProperties(typeof(Saint)).Find(property, true);
+
+
+            var saints = order == 
+                        Order.Ascending ? _context.Saints.ToList().OrderBy(x => prop.GetValue(x))
+                                        : _context.Saints.ToList().OrderByDescending(x => prop.GetValue(x));
+
+            // SYNTAX for parameterized ordering with queryables: OrderBy("Make ASC, Year DESC")
+            // ref: https://stackoverflow.com/questions/41244/dynamic-linq-orderby-on-ienumerablet-iqueryablet
+            // var saints = _context.Saints.AsQueryable().OrderBy($"{field} {orderby}").ToArray();
+            // var saints = _context.Saints.AsQueryable().ToArray().OrderByDescending(s => s.latestEvent);
 
             foreach (Saint s in saints)
             {
                 s.pictureUrl = _cloudinary.Api.UrlImgUp.Transform(new Transformation().Width(150).Height(200).Crop("fill")).BuildUrl("Trianairo/" + s.pictureUrl);
             }
-            
+
             var saintsToReturn = _mapper.Map<IEnumerable<SaintDto>>(saints);
-            
+
             return saintsToReturn;
         }
         [HttpGet("/saintsapi/{saintName}")]
@@ -63,9 +74,9 @@ namespace Trianairo.Controllers
         {
             var saint = _context.Saints
                     .FirstOrDefault(s => s.name == saintName);
-            
+
             return Ok(saint);
         }
-        
+
     }
 }
